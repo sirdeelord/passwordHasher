4fd821b32df7d8a9d3e1ca3c7894adfcc21d49e9
sirdeelord <saddyodus@yahoo.com>
2021-11-11 13:57:04 -0500

    comment rectify

--- PasswordHasher.cs
+++ PasswordHasher.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 namespace passwordHasher;
 public class PasswordHasher
 {
-    // hash password usisng PBKDF2 with SHA-256 with, 
+    // hash password using PBKDF2 with SHA-256 with
     // 128-bit salt and 1000 iterations
     public string HashPassword(string password)
     {
